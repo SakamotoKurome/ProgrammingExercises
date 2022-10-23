893f984b0c7b3126ffa8455882c446b369d7d90f
SakamotoKurome <sakamotokurome@gmail.com>
2022-10-23 21:13:48 +0800

    Ending today's MDN JavaScript study

--- MDN JS/test.js
+++ MDN JS/test.js
@@ -1,4 +1,14 @@
-addEventListener("message", message => {
-    if (message.data.command === 'generate') generatePrimes(message.data.quota);
-});
-postMessage(primes.length);
+const canvas = document.querySelector('.myCanvas');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+
+ctx.translate(width / 2, height / 2);
+const image = new Image();
+image.src = './walk-right.png';
+image.onload = draw;
+let sprite = 0;
+let posX = 0;
+function draw() {
+    ctx.fillRect
+}
