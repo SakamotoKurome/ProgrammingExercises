171d39d7e7fba8bdb0136f0a064a787022db162b
SakamotoKurome <sakamotokurome@gmail.com>
2023-01-22 21:59:49 +0800

    React Practice

--- react-app-scratch/src/index.js
+++ react-app-scratch/src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js"
+import Clock from "./Clock.js";
+import Toggle from "./Toggle.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <Toggle />
+    <Clock />
+    <App />
+  </>, 
+  document.getElementById("root"));
